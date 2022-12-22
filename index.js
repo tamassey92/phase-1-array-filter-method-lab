b47a5891d0936baeef799a4831e9a14f1bd13e15
Taylor Massey <tamassey92@gmail.com>
2022-12-21 22:06:02 -0700

    commit tree 6b0db54c8473028d699cd0dc73a02602598c86a0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// function findMatching(array,string){
+//     const results = array.filter(element => {
+//        if( element[0].toLowerCase()+element.slice(1,element.length)===string ||element=== string ){
+//             return console.log(string)
+//         }
+//     })
+        
+//     return results;
+// }
+
+function findMatching(array, string){
+    const results = array.filter(element => {
+        if (element.toLowerCase() === string)
+        return results
+    })
+}
